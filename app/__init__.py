70b5e18b6a5bc5b9cd94f59a10e8a005d6594a41
Nawan <z5527177@ad.edu.unsw.au>
2025-12-28 22:35:45 +0800

    added blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from app.config import Config
+from app.routes import blueprints
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
     CORS(app)
+    
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
         
     return app
---
+++ app/routes/__init__.py
@@ -0,0 +1,11 @@
+# Import all route blueprints
+from app.routes.meetings import bp as meetings_bp
+from app.routes.sessions import bp as sessions_bp
+from app.routes.drivers import bp as drivers_bp
+
+# List of all blueprints to register
+blueprints = [
+    meetings_bp,
+    sessions_bp,
+    drivers_bp
+]
