11c7d1d3bf44021d5a6665964ee5b7ca586d910e
Nawan <z5527177@ad.edu.unsw.au>
2025-12-29 18:38:07 +0800

    added endpoint for driver wins for a year

--- app/routes/drivers.py
+++ app/routes/drivers.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, make_response, request
-from app.models.driver_model import get_drivers_by_year
+from app.models.driver_model import get_drivers_by_year, get_driver_race_wins_in_year
 from app.utils.response_helper import create_response
 
 bp = Blueprint('drivers', __name__, url_prefix='/drivers')
@@ -8,4 +8,15 @@ bp = Blueprint('drivers', __name__, url_prefix='/drivers')
 def drivers_by_year():
     year = request.args.get('year')
     result, msg = get_drivers_by_year(year)
+    return create_response(result, msg)
+
+@bp.get('/race-wins')
+def driver_race_win_by_year():
+    driver_number = request.args.get('driver_number')
+    year = request.args.get('year')
+    result, msg = get_driver_race_wins_in_year(driver_number, year)
+    
+    if len(result) == 0:
+        return make_response(jsonify({'info': f'Driver did not win in the year {year}'}), 200)
+    
     return create_response(result, msg)
