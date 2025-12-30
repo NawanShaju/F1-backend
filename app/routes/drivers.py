d16f853ed9a9b249b588fb113d58f37a2d2a1455
Nawan <z5527177@ad.edu.unsw.au>
2025-12-30 12:59:25 +0800

    added validation to driver endpoint

--- app/routes/drivers.py
+++ app/routes/drivers.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, make_response, request
 from app.models.DriverModel import DriverModel
 from app.utils.response_helper import create_response
+from app.utils.Validator import Validator
 
 bp = Blueprint('drivers', __name__, url_prefix='/drivers')
 driver_model = DriverModel()
+validatior = Validator()
 
 @bp.get('/')
 def drivers_by_year():
@@ -15,6 +17,10 @@ def drivers_by_year():
 def driver_race_win_by_year():
     driver_number = request.args.get('driver_number')
     year = request.args.get('year')
+    
+    if not validatior.driver_exists_in_year(driver_number, year):
+        return make_response(jsonify({'error': f'Driver with number {driver_number} does not exist in the year {year}'}), 404)
+    
     result, msg = driver_model.get_driver_race_wins_in_year(driver_number, year)
     
     if len(result) == 0:
@@ -26,6 +32,10 @@ def driver_race_win_by_year():
 def driver_podiums_by_year():
     driver_number = request.args.get('driver_number')
     year = request.args.get('year')
+    
+    if not validatior.driver_exists_in_year(driver_number, year):
+        return make_response(jsonify({'error': f'Driver with number {driver_number} does not exist in the year {year}'}), 404)
+    
     result, msg = driver_model.get_driver_podiums_in_year(driver_number, year)
     
     if len(result) == 0:
